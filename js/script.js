085a7872371e748bb9f61842b77163d8829c33e7
Seraj007 <serajprivate123>
2022-08-26 15:03:37 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,131 @@
+
+
+
+;(function ($) {
+
+	'use strict';
+
+
+	$('.portfolio-single-slider').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+
+	});
+
+	$('.clients-logo').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.testimonial-wrap').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		infinite: true,
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },{
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  
+  		]
+	});
+
+	$(window).on('scroll', function () {
+		//.Scroll to top show/hide
+		if ($('#scroll-to-top').length) {
+			var scrollToTop = $('#scroll-to-top'),
+				scroll = $(window).scrollTop();
+			if (scroll >= 200) {
+				scrollToTop.fadeIn(200);
+			} else {
+				scrollToTop.fadeOut(100);
+			}
+		}
+	});
+		// scroll-to-top
+	if ($('#scroll-to-top').length) {
+		$('#scroll-to-top').on('click', function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	}
+
+   $('.portfolio-gallery').each(function () {
+        $(this).find('.popup-gallery').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+
+
+	// var map;
+
+	// function initialize() {
+	// 	var mapOptions = {
+	// 		zoom: 13,
+	// 		center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
+			
+	// 	};
+	// 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	// }
+
+	// var google_map_canvas = $('#map-canvas');
+
+	// if (google_map_canvas.length) {
+	// 	google.maps.event.addDomListener(window, 'load', initialize);
+	// }
+
+	// Counter
+
+	$('.counter-stat').counterUp({
+	      delay: 10,
+	      time: 1000
+	  });
+
+	  // typing text animation script
+	  var typed = new Typed(".typing", {
+        strings: ["People.", "Investment."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+})(jQuery);
